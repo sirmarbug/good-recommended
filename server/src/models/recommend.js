4bd32ba44ea63933fea72e5c0fcbd6372a5e1a47
Mariusz Bugajski <mariusz@bugajski.dev>
2022-08-06 18:06:54 +0200

    backend: fix bug

--- server/src/models/recommend.js
+++ server/src/models/recommend.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 
 const recommendSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
     type: { type: Number, required: true },
     name: { type: String, required: true },
     description: { type: String, default: null },

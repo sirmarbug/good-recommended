b00ac26a3b3e077856fb9aa26bd825175403fc27
Mariusz Bugajski <mariusz@bugajski.dev>
2022-08-06 12:03:01 +0200

    backend: add cors depend

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 require("./configs/db").connect()
 const express = require("express")
+const cors = require('cors')
 const auth = require("./routers/auth")
 const recommend = require("./routers/recommend")
 const authMiddleware = require("./middlewares/auth")
@@ -8,6 +9,7 @@ const authMiddleware = require("./middlewares/auth")
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', auth)
 app.use('/api/recommend', authMiddleware, recommend)

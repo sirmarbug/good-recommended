9c859f0a29127745296f6c1efa421a451a1f5fd2
Mariusz Bugajski <mariusz@bugajski.dev>
2022-08-13 20:27:52 +0200

    frontend: update navbar

--- client/src/layouts/DashboardLayout/DashboardLayoutNavigation.js
+++ client/src/layouts/DashboardLayout/DashboardLayoutNavigation.js
@@ -6,6 +6,25 @@ const Nav = styled.nav`
   background: #273444;
   color: #ebf0fc;
   padding: 1rem 0;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const NavContentLeft = styled.ul`
+  width: 60%;
+  display: flex;
+  justify-content: flex-start;
+  gap: 1rem;
+  padding: 0 5rem;
+`
+
+const NavContentRight = styled.ul`
+  width: 40%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 1rem;
+  padding: 0 5rem;
 `
 
 class DashboardLayoutNavigation extends Component{
@@ -13,32 +32,28 @@ class DashboardLayoutNavigation extends Component{
         return (
             <div>
                 <Nav>
-                    <ul>
-                        <li>
-                            <NavLink className="link" to="/dashboard">GoodRecommend</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/movies">Filmy</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/series">Seriale</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/books">Książki</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/recommend/details/123">Szczegóły</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/recommend/123/edit">Edycja</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/recommend/new">Dodaj</NavLink>
-                        </li>
-                        <li>
-                            <NavLink className="link" to="/login">Wyloguj</NavLink>
-                        </li>
-                    </ul>
+                    <NavContentLeft>
+                            <li>
+                                <NavLink className="link" to="/dashboard"><strong>GoodRecommend</strong></NavLink>
+                            </li>
+                            <li>
+                                <NavLink className="link" to="/movies">Filmy</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className="link" to="/series">Seriale</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className="link" to="/books">Książki</NavLink>
+                            </li>
+                    </NavContentLeft>
+                    <NavContentRight>
+                            <li>
+                                <NavLink className="link" to="/recommend/new">Dodaj</NavLink>
+                            </li>
+                            <li>
+                                <NavLink className="link" to="/login">Wyloguj</NavLink>
+                            </li>
+                    </NavContentRight>
                 </Nav>
             </div>
         )
